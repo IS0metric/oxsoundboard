4b3538f6316f4add146aefa0ca550d83eec44444
IS0metric <swgbhero@ymail.com>
2017-12-08 20:10:47 +0000

    Updated WSGI file

--- oxsoundboard/wsgi.py
+++ oxsoundboard/wsgi.py
@@ -9,7 +9,12 @@ https://docs.djangoproject.com/en/1.9/howto/deployment/wsgi/
 
 import os
 import sys
+import db_conf
 from django.core.wsgi import get_wsgi_application
+
+path = db_conf.WS_PATH
+if path not in sys.path:
+    sys.path.append(path)
 
 os.environ.setdefault("DJANGO_SETTINGS_MODULE", "oxsoundboard.settings")
 
