9b9d2bc69075261dc42338fcc3acacfd62c9800c
IS0metric <swgbhero@ymail.com>
2019-05-28 01:38:17 +0100

    Added Response import

--- app.py
+++ app.py
@@ -1,4 +1,4 @@
-from flask import Flask, render_template, send_from_directory, jsonify, request
+from flask import Flask, render_template, send_from_directory, jsonify, request, Response
 from flask_sqlalchemy import SQLAlchemy
 import db_conf
 import json
